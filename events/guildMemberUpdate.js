ac009d4f079f7b6a3a220cde055d1029ab00278a
Lion <caiofeliperro@gmail.com>
2020-09-17 16:56:16 -0300

    add guildMemberUpdate roles to Estúdio Raiz

---
+++ events/guildMemberUpdate.js
@@ -0,0 +1,24 @@
+module.exports = (client, oldMember, newMember) => {
+    if(oldMember.guild.id !== "505070509528973313") return;
+
+    function removeAllRoles(member, roles) {
+        if(!roles instanceof Object) return;
+        roles.forEach(role => {
+            if(member.roles.cache.has(role)) member.roles.remove(role)
+        })
+    }
+
+    const colors = ["660565736569176065","657386776205459473","657386775768989717","679458730533978137","679431011850780683","679430980221534217","679431096064147489","657386778524647435","657386777958416395","657386779774681109","657386779732869130","657386779648720926","657391307290968076","657391308758843427","657391308096274477","657391309539246080", "660516816740679681"];
+    const toVerify = ['721433478607929424', '642480977637277718', '554369669524357131','705935660036391097', '708094667312332840'];
+    let permission = false;
+
+    colors.forEach(colorID => {
+        toVerify.forEach(toVerify => {
+            if(!oldMember.roles.cache.has(colorID) && !newMember.roles.cache.has(colorID)) return;
+            if(newMember.roles.cache.has(toVerify)) permission = true;
+        })
+    })
+
+    if(permission) return;
+    else return removeAllRoles(newMember, colors);
+}
