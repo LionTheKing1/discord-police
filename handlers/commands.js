f6bd19e4b59020c49e5548ad3ab745533021e0a8
Lion <caiofeliperro@gmail.com>
2020-10-02 00:05:35 -0300

    testing

--- handlers/commands.js
+++ handlers/commands.js
@@ -52,9 +52,9 @@ module.exports = class commandHandler {
 
                 return message.channel.send(`${emoji.interruption} **|** ${message.author} ${messageToSay.join("\n\n")}`)
             }
+            console.log(serverConfig)
 
             if(serverConfig && (serverConfig.blockedChannels).includes(message.channel.id) && !message.member.hasPermission("MANAGE_MESSAGES") && !message.channel.permissionsFor(message.author).has("MANAGE_MESSAGES")) return message.react("739628740493181000");
-            console.log(serverConfig)
             console.log(`[LOGS] Command ${commandName} used in ${message.guild.id}`)
             
                     if (command.exclusiveCommand(message))
