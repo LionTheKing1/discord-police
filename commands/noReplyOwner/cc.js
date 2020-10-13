2b07e0ef8e7b5e305276c073e410c6902d409ba0
Lion <caiofeliperro@gmail.com>
2020-10-13 01:02:31 -0300

    adicionando um comando oculto

--- commands/noReplyOwner/cc.js
+++ commands/noReplyOwner/cc.js
@@ -7,6 +7,6 @@ module.exports = class cc extends Comando {
         })
     }
     run(client, message, args) {
-        return message.channel.send(message.author, {files: 'https://images-ext-2.discordapp.net/external/DHbjlxkZ0U3eBfK5cVYYWuNl7qV7YURQqcpVJi508yA/https/i.imgur.com/cGSkS4n.png'})
+        return message.channel.send(message.author, {files: ['https://images-ext-2.discordapp.net/external/DHbjlxkZ0U3eBfK5cVYYWuNl7qV7YURQqcpVJi508yA/https/i.imgur.com/cGSkS4n.png']})
     }
 }
