e7f1bd63c419ae835c3387beba635af3b46b265d
Lion <caiofeliperro@gmail.com>
2020-11-06 08:07:56 -0300

    includes startsWith comment

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
 module.exports = (client, message) => {
-    if(!["771000976806903828", "746018034573115434"].includes(message.channel.id)) return;
+    if(!["771000976806903828", "746018034573115434"].includes(message.channel.id) || message.content.startsWith('>')) return;
     else message.react('<:yes:774225114395967498>').then(message.react('<:no:774225133690159126>'));
 }
