ecc838acb479181836a27512781e47cb02d8fb49
Lion <caiofeliperro@gmail.com>
2020-11-26 20:47:32 -0300

    fix a word in command

--- handlers/commands.js
+++ handlers/commands.js
@@ -54,9 +54,9 @@ module.exports = class commandHandler {
             }
 
             if(serverConfig && serverConfig.blockedChannels.includes(message.channel.id) && !message.member.hasPermission("MANAGE_MESSAGES") && !message.channel.permissionsFor(message.author).has("MANAGE_MESSAGES")) return message.react("739628740493181000");
-            console.log(`[LOGS] Command ${commandName} used in ${message.guild.id}`)
+            console.log(`[LOGS] Command ${commandName} used in ${message.guild.id}`);
             
-                    if (command.exclusiveCommand(message))
+                if (command.exclusiveCommand(message))
                         if (command.enabled(message))
                             if((command.missArguments(args) && command.missArguments(args) > 0) || command.missArguments(args) == undefined) 
                                 if (command.botHasPermission(message))
