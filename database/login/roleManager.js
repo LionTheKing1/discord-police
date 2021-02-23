96acbada3f27f133db78581d47aed91c10d6705f
Lion <caiofeliperro@gmail.com>
2021-02-22 22:45:27 -0300

    fixing a bug

--- database/login/roleManager.js
+++ database/login/roleManager.js
@@ -26,7 +26,7 @@ module.exports = class tempRole {
    async ready() {
         for(const role in this.roles) {
             const toVerify = this.roles[role];
-            const guild = this.client.guilds.cache.get(toVerify.serverID);
+            const guild = await this.client.guilds.cache.get(toVerify.serverID);
             const member = guild.members.cache.get(toVerify.userID);
             if (!toVerify.expirado && member.roles.cache.has(toVerify.roleID)) continue;
 
