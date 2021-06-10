07e8615cc072441e429695c247b2954f04b03377
Lion <caiofeliperro@gmail.com>
2021-06-10 20:52:45 -0300

    add

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -9,9 +9,10 @@ module.exports = async (client, member) => {
 
     if(![member.guild.id].includes(GUILD_GAMING_SHOP_ID)) return;
     if(member.user.createdAt - Date.now() < MINIMUM_AGE_ACCOUNT) {
-        await member.send('Poxa, nos desculpe :(\n\n Por questões de segurança do Gaming Shop, sua conta deve ter uma idade mínima de **2 semanas**. Enquanto não chega, te removemos do servidor! \n\nTe esperamos lá em breve 👋🏻');
+        console.log('User Age:' + member.user.createdAt - Date.now() < MINIMUM_AGE_ACCOUNT)
+        /*await member.send('Poxa, nos desculpe :(\n\n Por questões de segurança do Gaming Shop, sua conta deve ter uma idade mínima de **2 semanas**. Enquanto não chega, te removemos do servidor! \n\nTe esperamos lá em breve 👋🏻');
         await member.guild.channels.cache.get(LOG_CHANNEL_ID).send(`Hey! I kicked the user **${member.user.tag} (${member.user.id})** because it hasn't reached the minimum age.`)
         await sleep(3_000);
-        return member.kick(`Account age isn't reached yet.`);
+        return member.kick(`Account age isn't reached yet.`);*/
     }
 }
