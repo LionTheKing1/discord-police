7f0c6bd5c790e1a1596beb6558be1cabfcbb1ebe
Lion <caiofeliperro@gmail.com>
2021-06-10 19:09:27 -0300

    New event gitMemberAdd

--- events/message.js
+++ events/message.js
@@ -1,6 +1,6 @@
 module.exports = async (client, message) => {
     if(message.guild.id == '505070509528973313') {
-    if(!["771000976806903828", "746018034573115434"].includes(message.channel.id) || message.content.startsWith('>')) return;
+    if(!["771000976806903828", "746018034573115434", "811959040459800588"].includes(message.channel.id) || message.content.startsWith('>')) return;
     else message.react('<:yes:774225114395967498>').then(message.react('<:no:774225133690159126>'));
     }
 
