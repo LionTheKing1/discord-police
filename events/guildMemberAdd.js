d133e0714940bcde37bcd0136b99e58d3d4f1a63
Lion <caiofeliperro@gmail.com>
2021-06-11 08:49:56 -0300

    It should work now

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -9,11 +9,13 @@ module.exports = async (client, member) => {
     const LOG_CHANNEL_ID = "674399261168893952";
     const MINIMUM_AGE_ACCOUNT = 1_209_600_000; // Minimum 2 weeks 
     const MEMBER_ACCOUNT_AGE = moment(member.user.createdAt).valueOf();
-    
+    const CALC_FINISHED = Date.now() - MEMBER_ACCOUNT_AGE;
+
     if(![member.guild.id].includes(GUILD_GAMING_SHOP_ID)) return;
-    if(MEMBER_ACCOUNT_AGE - Date.now() < MINIMUM_AGE_ACCOUNT) {
+    if(CALC_FINISHED < MINIMUM_AGE_ACCOUNT && member.kickable) {
+        console.log(`Kicked ${member.user.username}: ${CALC_FINISHED} ms`)
         await member.send('Poxa, nos desculpe :(\n\n Por questões de segurança do Gaming Shop, sua conta deve ter uma idade mínima de **2 semanas**. Enquanto não chega, te removemos do servidor! \n\nTe esperamos lá em breve 👋🏻');
-        await member.guild.channels.cache.get(LOG_CHANNEL_ID).send(`Hey! I kicked the user **${member.user.tag} (${member.user.id})** because it hasn't reached the minimum age.`)
+        await member.guild.channels.cache.get(LOG_CHANNEL_ID).send(`Hey! I kicked the user **${member.user.tag} (${member.user.id})** because it hasn't reached the minimum age.`);
         await sleep(3_000);
         return member.kick(`Account age isn't reached yet.`);
     }
